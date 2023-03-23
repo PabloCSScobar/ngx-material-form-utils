04a17b5bc3f08b4de564340eb89e6d384357cd47
PawelGajo <gpawel1997@gmail.com>
2023-03-23 23:10:37 +0100

    extends base input in textarea component

--- projects/ngx-material-form-utils/src/lib/textarea/textarea.component.ts
+++ projects/ngx-material-form-utils/src/lib/textarea/textarea.component.ts
@@ -1,8 +1,9 @@
 import { NgIf } from "@angular/common";
-import { Component, EventEmitter, Input, Output, SkipSelf, ViewChild } from "@angular/core";
+import { Component, Input, SkipSelf, ViewChild } from "@angular/core";
 import { ControlContainer, ReactiveFormsModule } from "@angular/forms";
-import { MatFormFieldAppearance, MatFormFieldModule } from "@angular/material/form-field";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInput, MatInputModule } from "@angular/material/input";
+import { MfuBaseInput } from "../input/base-input";
 import { MfuErrorMessage } from "../validation/error-message.component";
 
 @Component({
@@ -38,24 +39,9 @@ import { MfuErrorMessage } from "../validation/error-message.component";
     </mat-form-field>
   `
 })
-export class MfuTextareaComponent {
-  @Input() id!: string;
-  @Input() name!: string;
-  @Input() required = false;
-  @Input() readonly = false;
-  @Input() value: any;
-  @Input() controlName!: string;
-  @Input() appearance: MatFormFieldAppearance = 'outline';
-  @Input() showErrors = true;
-  @Input() label?: string | null = null;
-  @Input() placeholder: string = '';
-  @Input() hint?: string | null = null;
+export class MfuTextareaComponent extends MfuBaseInput {
   @Input() rows?: number | null = null;
   @Input() cols?: number | null = null;
 
-  @Output() blur = new EventEmitter<FocusEvent>();
-  @Output() focus = new EventEmitter<FocusEvent>();
-
-  @ViewChild(MatInput) matInput!: MatInput;
-  
+  @ViewChild(MatInput) matInput!: MatInput;  
 }
